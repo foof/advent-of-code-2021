96b5eb08fe40fe5cb61055abf5506a79bb29d001
Jonas Holmström <jonas@holmstrom.me>
2021-12-02 08:31:52 +0100

    consistency

--- 1/2.py
+++ 1/2.py
@@ -1,7 +1,7 @@
 
 def solve(lines):
     windows = [int(line) + int(lines[i+1]) + int(lines[i+2]) for i, line in enumerate(lines) if i+2 < len(lines)]
-    return sum([1 for i, window in enumerate(windows) if i < len(windows)-1 and window < windows[i+1]])
+    return sum([1 for i, window in enumerate(windows) if i+1 < len(windows) and window < windows[i+1]])
 
 
 if __name__ == "__main__":

d8523d4358f12c1fa03262f1042615df59d0bacc
Jonas Holmström <jonas@holmstrom.me>
2021-12-16 08:22:10 +0100

    Day 15 - Added heuristic function and min-heap

--- 15/2.py
+++ 15/2.py
@@ -1,5 +1,5 @@
+import heapq
 import math
-from collections import deque
 
 
 def reconstruct_path(came_from, current, nodes):
@@ -15,14 +15,27 @@ def cost_of_path(path, nodes):
 
 
 def solve(start, end, nodes):
-    open_set = deque([start])
+    max_x, max_y = 0, 0
+    for node in nodes:
+        max_x = max(max_x, node[0])
+        max_y = max(max_y, node[1])
+
+    def h(node):
+        return abs(max_x-node[0]) + abs(max_y-node[0])
+
+    open_set = [(0, start[0], start[1])]
+    heapq.heapify(open_set)
+
     came_from = {}
     g_score = {node: math.inf for node in nodes.keys()}
     g_score[start] = 0
+    f_score = {node: math.inf for node in nodes.keys()}
+    f_score[start] = h(start)
     n_deltas = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
     while len(open_set):
-        current = open_set.popleft()
+        current = heapq.heappop(open_set)
+        current = (current[1], current[2])
         if current == end:
             return reconstruct_path(came_from, current, nodes)
 
@@ -35,8 +48,9 @@ def solve(start, end, nodes):
             if tentative_g_score < g_score[neighbour]:
                 came_from[neighbour] = current
                 g_score[neighbour] = tentative_g_score
+                f_score[neighbour] = tentative_g_score + h(neighbour)
                 if neighbour not in open_set:
-                    open_set.append(neighbour)
+                    heapq.heappush(open_set, (f_score[neighbour], neighbour[0], neighbour[1]))
 
     return 'failed'
 

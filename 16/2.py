ee51b7f5aa066ca479dfb259619a5d46b38a195c
Jonas Holmström <jonas@holmstrom.me>
2021-12-16 16:58:16 +0100

    Day 16 - cleanup

--- 16/2.py
+++ 16/2.py
@@ -30,10 +30,7 @@ def handle_value(type, value, new_value):
 
 
 def parse_next_packet(bits):
-    version = bits[0:3]
-    type = bits[3:6]
-    version = int(version, 2)
-    type = int(type, 2)
+    type = int(bits[3:6], 2)
     value = None
 
     if type == 4:
@@ -41,44 +38,47 @@ def parse_next_packet(bits):
         offset = 6
         while True:
             is_last = int(bits[offset:offset+1]) == 0
-            val = bits[offset+1:offset+5]
-            literal += val
+            literal += bits[offset+1:offset+5]
             offset += 5
             if is_last:
                 value = int(literal, 2)
                 break
         L = offset
     else:
-        I = int(bits[6:7])
-        if I:
+        length_type = int(bits[6:7])
+        if length_type:
             num_subpackets = int(bits[7:18], 2)
             L = 18
+            sub_bits = bits[L:]
             for _ in range(num_subpackets):
-                _, new_L, v = parse_next_packet(bits[L:])
-                value = handle_value(type, value, v)
+                new_L, new_value = parse_next_packet(sub_bits)
+                value = handle_value(type, value, new_value)
                 L += new_L
+                sub_bits = bits[L:]
         else:
             subpacket_length = int(bits[7:22], 2)
             L = 22 + subpacket_length
             sub_bits = bits[22:L]
             while len(sub_bits):
-                sub_bits, _, v = parse_next_packet(sub_bits)
-                value = handle_value(type, value, v)
+                new_L, new_value = parse_next_packet(sub_bits)
+                value = handle_value(type, value, new_value)
+                sub_bits = sub_bits[new_L:]
 
-    return bits[L:], L, value
+    return L, value
 
 
 def solve(line):
     bin_length = len(line)*4
     spec = '{fill}{align}{width}{type}'.format(fill='0', align='>', width=bin_length, type='b')
     hex = int(line, 16)
-    bits = format(hex, spec)
-    val = None
+    remaining_bits = format(hex, spec)
+    value = None
 
-    while len(bits) > 7:
-        bits, L, val = parse_next_packet(bits)
+    while len(remaining_bits) > 7:
+        L, value = parse_next_packet(remaining_bits)
+        remaining_bits = remaining_bits[L:]
 
-    return val
+    return value
 
 
 if __name__ == "__main__":
